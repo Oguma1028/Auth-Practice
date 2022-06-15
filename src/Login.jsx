e3d6502fd1814278d6bed4e5fd0829646b8580de
Oguma1028 <neetoguma@gmail.com>
2022-06-15 12:00:00 +0900

    useStateを設定

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,17 +1,32 @@
 import React from "react";
+import { useState } from "react";
 
 const Login = () => {
+  const [loginEmail, setLoginEmail] = useState("");
+  const [loginPassWord, setLoginPassWord] = useState("");
   return (
     <>
       <h1>ログインページ</h1>
       <form>
         <div>
           <label>メールアドレス</label>
-          <input name="email" type="email" />
+          <input
+            name="email"
+            type="email"
+            value={loginEmail}
+            onChange={(e) => {
+              setLoginEmail(e.target.value);
+            }}
+          />
         </div>
         <div>
           <label>パスワード</label>
-          <input name="password" type="password" />
+          <input
+            name="password"
+            type="password"
+            value={loginPassWord}
+            onChange={(e) => setLoginPassWord(e.target.value)}
+          />
         </div>
         <button>ログイン</button>
       </form>
--- src/register.jsx
+++ src/register.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import { useState } from "react";
 
 const Register = () => {
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassWord, setRegisterPassWord] = useState("");
   return (
     <>
       <h1>新規登録</h1>
       <form>
         <div>
           <label>メールアドレス</label>
-          <input name="email" type="email" />
+          <input
+            name="email"
+            type="email"
+            value={registerEmail}
+            onChange={(e) => setRegisterEmail(e.target.value)}
+          />
         </div>
         <div>
           <label>パスワード</label>
-          <input name="password" type="password" />
+          <input
+            name="password"
+            type="password"
+            value={registerPassWord}
+            onChange={(e) => setRegisterPassWord(e.target.value)}
+          />
         </div>
         <button>登録する</button>
       </form>
